dc59ab5223cd8642bd38f36f876e990da3be8b46
hayhaydz <haydon1900@gmail.com>
2020-03-13 11:00:53 +0000

    Title and Begin Button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,15 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+      <div className="App__left">
+        <h1 className="App__left--title App__left--title_top">Student</h1>
+        <h1 className="App__left--title App__left--title_bottom">Meals</h1>
+        <button className="App__left--btn">
+          <span>Begin your journey</span> 
+          <i className="material-icons">arrow_forward</i>
+        </button>
+      </div>
+      <div className="App__border"></div>
     </div>
   );
 }
