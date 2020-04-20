828bc158acc330a80986082e1ba5e91ddc287b26
hayhaydz <haydon1900@gmail.com>
2020-04-20 20:20:39 +0100

    Updates

---
+++ precache-manifest.36d11a9c0d7d693fa60c5ea1dd5a6a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dece67647d13c62a5a641ef79fd4cf",
+    "url": "/hypertext-story/index.html"
+  },
+  {
+    "revision": "1b3eac0946dc1adf03f5",
+    "url": "/hypertext-story/static/css/main.f8b1cb81.chunk.css"
+  },
+  {
+    "revision": "316246601937658df5be",
+    "url": "/hypertext-story/static/js/2.fc9fe512.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/hypertext-story/static/js/2.fc9fe512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3eac0946dc1adf03f5",
+    "url": "/hypertext-story/static/js/main.5927bc9d.chunk.js"
+  },
+  {
+    "revision": "4f50c09891f0810851d6",
+    "url": "/hypertext-story/static/js/runtime-main.16f2fee5.js"
+  }
+]);
